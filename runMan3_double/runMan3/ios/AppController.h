3f9192aab53ce3f7246e3441e332a52016af4121
crazylitm <litianmin@gmail.com>
2015-06-07 20:09:45 +0800

    add runman3_double by other dir

---
+++ runMan3_double/runMan3/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  runMan3AppController.h
+//  runMan3
+//
+//  Created by crazywork on 14-2-27.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
